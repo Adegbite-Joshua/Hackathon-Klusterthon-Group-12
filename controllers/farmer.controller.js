b1f44a7ade2c2a442d7e226e0dd20393dd275887
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-11-21 21:55:18 +0100

    Created the password reset links

--- controllers/farmer.controller.js
+++ controllers/farmer.controller.js
@@ -1,5 +1,20 @@
 const { farmerDetailsModel } = require("../models/farmer.model");
 const { verify } = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+
+const USER_EMAIL = process.env.USER_EMAIL;
+const USER_PASSWORD = process.env.USER_PASSWORD;
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: USER_EMAIL,
+        pass: USER_PASSWORD
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
+
+})
 
 // Function For The Famer's Create Account
 const createAccount = (req, res) => {
@@ -15,8 +30,7 @@ const createAccount = (req, res) => {
         })
 }
 
-
-// Functions For The Farmer's Sign In
+// Function For The Farmer's Sign In
 const signIn = (req, res) => {
     console.log(req.body);
     let { password, email } = req.body
@@ -42,9 +56,11 @@ const signIn = (req, res) => {
         })
 }
 
+// Function For Getting The Farmer's Details
 const getFarmerDetails = (req, res) => {
     let {token} = req.body;
-    verify(token, process.env.JWTSECRET, (error, result) => {
+    const JWT_SECRET = process.env.JWT_SECRET;
+    verify(token, JWT_SECRET, (error, result) => {
         if (!error) {
             farmerDetailsModel.findOne({email: result.email})
             .then((details)=>{
@@ -59,4 +75,70 @@ const getFarmerDetails = (req, res) => {
     })
 }
 
-module.exports = {createAccount, signIn, getFarmerDetails}
+const sendResetPasswordLink = (req, res) => {
+    let { email } = req.body;
+    farmerDetailsModel.findOne({email}, {firstName: 1})
+    .then((details)=>{
+        if(details == null){
+            return res.status(478).json({message: 'Invalid Email'})
+        }
+        let token = jwt.sign({ email }, JWT_SECRET, { expiresIn: '1h' })
+        let mailOptions = {
+            from: process.env.USEREMAIL,
+            to: [email],
+            subject: 'Pasword Reset',
+            html: `<h1 style='text-align:center'>Pasword Reset</h1>
+
+                    <p style='text-align:center'>Dear ${details.firstName}, you request for a password reset link</p>
+                    <a href='token'><button style='text-align:center; background-color: blue;'>Click Here</button></a>
+                    <p style='text-align:center'>This link expires in the next 1 hour</p>
+                    
+                    <p style='text-align:center; background-color: blue;'><small>We are glad to have you!</small></p>`
+        }
+        transporter.sendMail(mailOptions)
+        .then((response)=>{
+            console.log(response)
+            res.status(200).json({message: 'Email Sent'})
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    })
+}
+
+const verifyResetEmail = (req, res) => {
+    let { token } = req.body;
+    const JWT_SECRET = process.env.JWT_SECRET;
+    verify(token, JWT_SECRET, (error, result) => {
+        if (!error) {
+            farmerDetailsModel.findOne({email: result.email}, {firstName: 1, lastName: 1})
+            .then((details)=>{
+                res.status(200).json({ details })
+            })
+            .catch((err)=>{
+                console.error(err)
+            })            
+        } else {
+            res.status(476).json({ message: 'Invalid Token' })
+        }
+    })
+}
+
+const resetPassword = (req, res) => {
+    let {token, password} = req.body;
+    const JWT_SECRET = process.env.JWT_SECRET;
+    verify(token, JWT_SECRET, (error, result) => {
+        if (!error) {
+            farmerDetailsModel.findOneAndUpdate({email: result.email}, {password})
+            .then((details)=>{
+                res.status(200).json({ message: 'Successful' })
+            })
+            .catch((err)=>{
+                console.error(err)
+            })            
+        } else {
+            res.status(476).json({ message: 'Invalid Token' })
+        }
+    })
+}
+module.exports = {createAccount, signIn, getFarmerDetails, sendResetPasswordLink, verifyResetEmail, resetPassword}
--- routes/farmer.route.js
+++ routes/farmer.route.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { createAccount, signIn, getFarmerDetails } = require('../controllers/farmer.controller');
+const { createAccount, signIn, getFarmerDetails, sendResetPasswordLink, verifyResetEmail, resetPassword } = require('../controllers/farmer.controller');
 const router = express.Router();
 
 router.post('/create_account', createAccount)
 router.post('/sign_in', signIn)
 router.post('/details', getFarmerDetails)
+router.post('/request_reset_password_link', sendResetPasswordLink)
+router.post('/validate_reset_password_email', verifyResetEmail)
+router.post('/reset_password', resetPassword)
 
 
 module.exports = router;
